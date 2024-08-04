7d4293dd603b06a4be092b50e0a8f7f2d665b6b1
Saurav12-7 <ks7835709@gmail.com>
2024-08-04 20:55:05 +0530

    Update ProductForm.js

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -4,44 +4,56 @@ import { useState } from 'react';
 
 function ProductForm(props)
 {
-    const[NewTitle,setTitle]=useState('');
-    const[NewDate,setDate]=useState('');
+    // const[NewTitle,setTitle]=useState('');
+    // const[NewDate,setDate]=useState('');
+    const [newInfo,setNewInfo]=useState(
+        {
+            NewTitle:"",
+            NewDate:""
+        }
+    );
 
-    function titleHandler(event)
+    function NewInfo(e)
     {
-        setTitle(event.target.value);
+        setNewInfo(prevState=>({ ...prevState, [e.target.name]: e.target.value }));
     }
+    // function titleHandler(event)
+    // {
+    //     setTitle(event.target.value);
+    // }
 
-    function dateHandler(event)
-    {
-        setDate(event.target.value);
-    }
+    // function dateHandler(event)
+    // {
+    //     setDate(event.target.value);
+    // }
 
     function submitHandler(event)
     {
         event.preventDefault();
-        let date=NewDate.split("-");
+        let date=newInfo?.NewDate.split("-");
         let obj={
-            item_name:NewTitle,
+            item_name:newInfo.NewTitle,
             mfg_date:parseInt(date[2]),
             mfg_month:parseInt(date[1]),
             mfg_year:parseInt(date[0])
         }
         props.object(obj);
-        setTitle('');
-        setDate('');
+        setNewInfo({
+            NewTitle:"",
+            NewDate:""
+        })
 
     }
     return (
         <form onSubmit={submitHandler}>
             <div className='input'>
                 <label>Title </label>
-                <input type="text" value={NewTitle} onChange={titleHandler}></input>
+                <input type="text" value={newInfo.NewTitle} onChange={NewInfo} name='NewTitle'></input>
             </div>
 
             <div className='input'>
                 <label>Date </label>
-                <input type="date" value={NewDate} onChange={dateHandler}></input>
+                <input type="date" value={newInfo.NewDate} onChange={NewInfo} name='NewDate'></input>
             </div>
 
             <div className='btn'>
